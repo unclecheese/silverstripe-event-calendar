3d82e42d495a105576ba7489e2a2e0b2d3ee0eec
unclecheese <unclecheese@leftandmain.com>
2012-08-01 13:39:18 -0300

    Added infinite scroll

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,6 +1,10 @@
 (function($) {
 $(function() {
+
+	var fetching = false;
+
 	$('.calendar-view-more').live("click",function(e) {
+		fetching = true;
 		e.preventDefault();
 		$(this).addClass('loading');
 		var $t = $(this);
@@ -9,11 +13,20 @@ $(function() {
 			success: function(data) {
 				$t.remove();
 				$('#events').append(data);
+				fetching = false;
 			}
 
 		})
 	});
 
+
+	$(window).scroll(function() {
+		if ($(window).scrollTop() >= ($(document).height() - $(window).height())) {	
+			if ($('.calendar-view-more').length && !fetching) {      	
+				$('.calendar-view-more').click();
+			}        
+		}
+	});
 });
 
 })(jQuery);
