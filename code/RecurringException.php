16c2cd9a3c81ef7e5cab21b34104b9be0e4a3857
Ian Walls <mail.sekjal@gmail.com>
2013-05-29 10:47:43 -0400

    Issue #11: Recurrence Exceptions don't work
    Uncommented Recurrence Exceptions in Calendar Event, and cleaned up for
    usability.
    
    1. Show dates instead of IDs in Gridfield
    2. Use a calendar to select Exception Date

--- code/RecurringException.php
+++ code/RecurringException.php
@@ -14,5 +14,27 @@ class RecurringException extends DataObject
 
 
 	static $default_sort = "ExceptionDate ASC";
-	
+
+        public function getCMSFields() {
+                DateField::set_default_config('showcalendar', true);
+                $f = new FieldList(
+                        new DateField('ExceptionDate',_t('CalendarDateTime.EXCEPTIONDATE','Exception Date'))
+                );
+
+                $this->extend('updateCMSFields', $f);
+
+                return $f;
+        }
+
+       public function summaryFields() {
+                return array (
+                        'FormattedExceptionDate' => _t('Calendar.EXCEPTIONDATE','Exception date')
+                );
+        }
+
+        public function getFormattedExceptionDate() {
+           if(!$this->ExceptionDate) return "--";
+           return CalendarUtil::get_date_format() == "mdy" ? $this->obj('ExceptionDate')->Format('m-d-Y') : $this->obj('ExceptionDate')->Format('d-m-Y');
+        }
+
 }
