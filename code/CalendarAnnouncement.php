07e7006b745e28ec9e21c1329a8066c1e0234e73
Zigterman <remco.zigterman@topicus.nl>
2014-02-12 10:29:34 +0100

    Add summaryFields toe CalendarAnnouncement

--- code/CalendarAnnouncement.php
+++ code/CalendarAnnouncement.php
@@ -21,6 +21,17 @@ class CalendarAnnouncement extends CalendarDateTime {
 		return $f;
 	}
 
+	public function summaryFields() {
+		return array (
+				'Title' => _t('CalendarAnnouncement.TITLE','Title of announcement'),
+				'FormattedStartDate' => _t('Calendar.STARTDATE','Start date'),
+				'FormattedEndDate' => _t('Calendar.ENDDATE','End date'),
+				'FormattedStartTime' => _t('Calendar.STARTTIME','Start time'),
+				'FormattedEndTime' => _t('Calendar.ENDTIME','End time'),
+				'FormattedAllDay' => _t('Calendar.ALLDAY','All day'),
+		);
+	}
+	
 	public function getTitle() {
 		return $this->getField('Title');
 	}
