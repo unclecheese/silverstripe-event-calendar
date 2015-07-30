212c7a151a46000d8b49367f5773aae9426e80ce
Bruce <bruce@alfgundersen.no>
2015-07-30 13:12:18 +0200

    Added Norwegian translation

---
+++ javascript/lang/calendar_nb.js
@@ -0,0 +1,8 @@
+(function($) {
+    $.CalendarWidget.setOptions({
+        startOnMonday: true,
+        calDaysLabels: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'],
+        calMonthsLabels: ['Januar', 'Februar', 'Mars', 'April','Mai', 'Juni', 'Juli', 'August', 'September','Oktober', 'November', 'Desember']
+
+    })
+})(jQuery);
---
+++ javascript/locale/date_nb.js
@@ -0,0 +1,47 @@
+/**
+ * An Array of day names starting with Sunday.
+ * 
+ * @example dayNames[0]
+ * @result 'Sunday'
+ *
+ * @name dayNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.dayNames = ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'];
+
+/**
+ * An Array of abbreviated day names starting with Sun.
+ * 
+ * @example abbrDayNames[0]
+ * @result 'Sun'
+ *
+ * @name abbrDayNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.abbrDayNames = ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'];
+
+/**
+ * An Array of month names starting with Janurary.
+ * 
+ * @example monthNames[0]
+ * @result 'January'
+ *
+ * @name monthNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.monthNames = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'];
+
+/**
+ * An Array of abbreviated month names starting with Jan.
+ * 
+ * @example abbrMonthNames[0]
+ * @result 'Jan'
+ *
+ * @name monthNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.abbrMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'];
